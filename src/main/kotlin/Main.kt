625abbce2e58d2a1621119baa09a19819365c519
Alex Ashworth <alexashworth88@gmail.com>
2023-09-14 16:03:50 -0400

    initial commit, solution for day 1 parts 1 & 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,59 @@
 import java.io.File
 
-val inputFileNameProd = "/Users/alexashworth/Desktop/AdventOfCodeKotlin/src/main/kotlin/input.txt"
-val inputFileNameTest = "/Users/alexashworth/Desktop/AdventOfCodeKotlin/src/main/kotlin/testInput.txt"
+val inputFileProd = "/Users/alexashworth/Desktop/AdventOfCode/AdventOfCodeKotlin/src/main/kotlin/input.txt"
+val inputFileTest = "/Users/alexashworth/Desktop/AdventOfCode/AdventOfCodeKotlin/src/main/kotlin/testInput.txt"
 
 fun main(args: Array<String>) {
-    getParsedInputFile(inputFileNameTest)
+    println(getSumOfHighestNCalories(3, inputFileProd))
 }
 
-fun getParsedInputFile(inputFileName: String): ArrayList<Int> {
+fun getSumOfHighestNCalories(numberOfElves: Int, inputFileName: String ): Int {
+    return getListOfHighestIndividualCalories(numberOfElves, inputFileName).sum()
+}
+
+fun getListOfHighestIndividualCalories(numberOfElves: Int, inputFileName: String ): List<Int> {
+    val parsedInputList = getParsedInputList(inputFileName)
+    val talliedCalorieList = getTalliedCalorieList(parsedInputList)
+
+    return talliedCalorieList.sortedDescending().subList(0, numberOfElves)
+}
+
+fun getTalliedCalorieList(inputList: ArrayList<Int>): ArrayList<Int> {
+    val talliedCalorieList: ArrayList<Int> = ArrayList<Int>()
+    var currentTally = 0
+
+    inputList.forEachIndexed { index, calories ->
+        if(calories.equals(0) && currentTally > 0 ) {
+            talliedCalorieList.add(currentTally)
+            currentTally = 0
+        } else {
+            currentTally += calories
+
+            // corner case: last element
+            if(index.equals(inputList.lastIndex)) talliedCalorieList.add(currentTally)
+        }
+    }
+
+    return talliedCalorieList
+}
+
+fun getParsedInputList(inputFileName: String): ArrayList<Int> {
     val inputArray: ArrayList<Int> = ArrayList<Int>()
 
     try {
         val file = File(inputFileName)
         if (file.exists()) {
             val lines = file.readLines()
-            // separate out blank lines for next time
+
             for (line in lines) {
                 try {
-                    val intValue = line.trim().toInt()
-                    println("Adding $intValue to array")
+                    if(line.trim().isBlank()) {
+                        inputArray.add(0)
+                    } else {
+                        val intValue = line.trim().toInt()
+                        inputArray.add(intValue)
+                    }
+
                 } catch (e: NumberFormatException) {
                     println("Error: Invalid integer format: $line will not be saved")
                 }
@@ -33,6 +68,11 @@ fun getParsedInputFile(inputFileName: String): ArrayList<Int> {
     return inputArray
 }
 
+fun printList(list: List<Int>) {
+    list.forEach {
+        println(it)
+    }
+}
 
 
 
